77a2811760b94b4a7d94ebef481a3d6ccd5dedef
Niklas Sandén <ssandenniklas@gmail.com>
2019-08-31 22:32:28 +0200

    More textures

--- ProceduralGeneration/src/main.cpp
+++ ProceduralGeneration/src/main.cpp
@@ -134,18 +134,20 @@ int main() {
 	// create shader program
 	Shader ourShader({ ShaderFileHandler::getShaderID("shader.vert"), ShaderFileHandler::getShaderID("shader.frag") });
 
-
+	
 	// --------------------------------------------------------------------------
 	// Rendering setup
 	// --------------------------------------------------------------------------
 	float vertices[] = {
-		// positions         // colors
-		 0.98f, -0.85f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-		-0.98f, -0.85f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-		 0.0f,  0.85f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 	unsigned int indices[]{
-		0, 1, 2
+		0, 1, 3,
+		1, 2, 3
 	};
 
 	// Generate buffers
@@ -167,12 +169,69 @@ int main() {
 
 	// Vertex attributes
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// colour attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	// texture coordinates
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	// load and create a texture
+	// -------------------------
+	stbi_set_flip_vertically_on_load(true);
+	// texture1
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("res/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// texture2
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	int width2, height2, nrChannels2;
+	unsigned char* data2 = stbi_load("res/awesomeface.png", &width2, &height2, &nrChannels2, 0);
+	if (data2)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data2);
+
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -181,6 +240,13 @@ int main() {
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
+	//glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	ourShader.use();
+	ourShader.setInt("texture0", 0);
+	ourShader.setInt("texture1", 1);
+	glUseProgram(0);
 
 
 	// wireframe mode
@@ -212,6 +278,12 @@ int main() {
 		
 		// set shader program
 		ourShader.use();
+
+		// textures
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		// render
 		glBindVertexArray(VAO);
--- ProceduralGeneration/src/shaders/shader.frag
+++ ProceduralGeneration/src/shaders/shader.frag
@@ -2,8 +2,13 @@
 out vec4 FragColour;
 
 in vec3 ourColour;
+in vec2 texCoord;
+
+// textures
+uniform sampler2D texture0;
+uniform sampler2D texture1;
 
 void main()
 {
-    FragColour = vec4(ourColour, 1.0f);
+    FragColour = mix(texture(texture0, texCoord), texture(texture1, vec2(1 - texCoord.x, texCoord.y)), 0.5);
 }
