1b3a05ec65e16d2f4237f7a63ffab5e3b8615b30
NiklasSanden <Lolfnaw@gmail.com>
2019-09-06 03:42:37 +0200

    ImGui Docking

--- ProceduralGeneration/src/engine/ImGUILayer.cpp
+++ ProceduralGeneration/src/engine/ImGUILayer.cpp
@@ -1,5 +1,7 @@
 #include "ImGuiLayer.h"
 
+#include "GLFW/glfw3.h"
+
 #include "Debug.h"
 using namespace Engine;
 
@@ -11,10 +13,20 @@ ImGuiLayer::ImGuiLayer(GLFWwindow* window) {
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NoMouse;				  // Disable mouse interatcion (since we are using glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); at the start
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
 	//ImGui::StyleColorsClassic();
+
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
 
 	// Setup Platform/Renderer bindings
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -42,4 +54,16 @@ void ImGuiLayer::end() {
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	// ----------------------------------
+
+	ImGuiIO& io = ImGui::GetIO();
+	// Update and Render additional Platform Windows
+	// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+	//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		GLFWwindow* backup_current_context = glfwGetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		glfwMakeContextCurrent(backup_current_context);
+	}
 }
